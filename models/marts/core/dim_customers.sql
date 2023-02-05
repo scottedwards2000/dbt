47020d976c678b088529ce6e3a19e12c5a77a5c0
Scott Edwards <scottedwards2000@gmail.com>
2023-02-05 06:39:56 +0000

    update dim_customers to use fct_orders

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -2,20 +2,17 @@
 with
     customers as (select * from {{ ref("stg_customers") }}),
 
-    orders as (select * from {{ ref("stg_orders") }}),
+    orders as (select * from {{ ref("fct_orders") }}),
 
     customer_orders as (
 
         select
             o.customer_id,
-
             min(o.order_date) as first_order_date,
             max(o.order_date) as most_recent_order_date,
             count(o.order_id) as number_of_orders,
             sum(p.amount) as lifetime_value
-
-        from orders o join {{ ref('stg_payments') }} p on o.order_id = p.order_id
-
+        from orders o
         group by 1
 
     ),
@@ -29,9 +26,7 @@ with
             co.most_recent_order_date,
             coalesce(co.number_of_orders, 0) as number_of_orders,
             co.lifetime_value
-
         from customers c
-
         left join customer_orders co on c.customer_id = co.customer_id
 
     )
