dc5538626039dcb7807333f4bd08ffb3ccd1c233
Scott Edwards <scottedwards2000@gmail.com>
2023-02-05 01:15:53 +0000

    changed left join to inner

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,2 +1,2 @@
 select o.order_id, o.customer_id, p.amount
-from {{ ref('stg_orders') }} o left join {{ ref('stg_payments') }} p on o.order_id = p.order_id
+from {{ ref('stg_orders') }} o join {{ ref('stg_payments') }} p on o.order_id = p.order_id
