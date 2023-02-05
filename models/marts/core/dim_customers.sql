dff521fd7cd079f4f9cfd627e3cf962b364164b3
Scott Edwards <scottedwards2000@gmail.com>
2023-02-05 02:25:41 +0000

    added lifetime value

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -7,13 +7,14 @@ with
     customer_orders as (
 
         select
-            customer_id,
+            o.customer_id,
 
-            min(order_date) as first_order_date,
-            max(order_date) as most_recent_order_date,
-            count(order_id) as number_of_orders
+            min(o.order_date) as first_order_date,
+            max(o.order_date) as most_recent_order_date,
+            count(o.order_id) as number_of_orders,
+            sum(p.amount) as lifetime_value
 
-        from orders
+        from orders o join {{ ref('stg_payments') }} p on o.order_id = p.order_id
 
         group by 1
 
@@ -21,16 +22,17 @@ with
     final as (
 
         select
-            customers.customer_id,
-            customers.first_name,
-            customers.last_name,
-            customer_orders.first_order_date,
-            customer_orders.most_recent_order_date,
-            coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+            c.customer_id,
+            c.first_name,
+            c.last_name,
+            co.first_order_date,
+            co.most_recent_order_date,
+            coalesce(co.number_of_orders, 0) as number_of_orders,
+            co.lifetime_value
 
-        from customers
+        from customers c
 
-        left join customer_orders using (customer_id)
+        left join customer_orders co on c.customer_id = co.customer_id
 
     )
 select *
