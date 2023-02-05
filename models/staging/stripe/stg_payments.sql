02afebaa3def849031b98371bb411c26eee38bec
Scott Edwards <scottedwards2000@gmail.com>
2023-02-05 06:05:17 +0000

    fixed 100 cents

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -3,6 +3,6 @@ select
     orderid as order_id,
     paymentmethod as payment_method,
     status,
-    amount,
-    created
+    amount/100 as amount,
+    created as created_at
 from `dbt-tutorial.stripe.payment`
