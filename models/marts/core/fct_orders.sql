e43f74768f9f1a1591c472b21210101fe97f7072
scottedwards2000@gmail.com <scottedwards2000@gmail.com>
2023-02-19 02:03:00 +0000

    didn't need inner but i did add status is null

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,5 +1,6 @@
-select o.order_id, o.customer_id, o.order_date, coalesce(sum(p.amount), 0) as amount
+select o.order_id, o.customer_id, o.order_date
+     , coalesce(sum(p.amount), 0) as amount
 from {{ ref("stg_orders") }} o
 left join {{ ref("stg_payments") }} p on o.order_id = p.order_id
-where p.status = 'success'
+where p.status = 'success' OR p.status is null
 group by 1, 2, 3
